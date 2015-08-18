0aa7a25592dfe078d6622e1c4813297045c808f9
wilsonchaney <wilsongchaney@gmail.com>
2015-08-17 20:47:36 -0500

    quick scraping function

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 from player_list_scraper import get_all_players
 import json
 
-players = get_all_players()
-with open("player_list.txt","w") as file:
-    json.dump([p.__dict__ for p in players],file);
+# Should really only need to be run one time - DLs player data from site
+def scrape_player_list(file_name):
+    players = get_all_players()
+    with open("player_list.txt","w") as file:
+        json.dump([p.__dict__ for p in players],file)
